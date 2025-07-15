3a27f0c63bd033e37652a6e1377f6227ad6803e7
gad-alt <hirwagad767@gmail.com>
2025-07-15 18:32:41 +0200

    feat: create html document

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <div className="navbar-container">
+      <button id="navbar-home">home</button>
+      <button id="navbar-add">add student</button>
+      <button id="navbar-all">all students</button>
+    </div>
+  );
 };
-
 export default Navbar;
