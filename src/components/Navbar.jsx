461000163648c56690505d8eb52f311624771ab2
gad-alt <hirwagad767@gmail.com>
2025-07-16 19:34:50 +0200

    feat: improve performance

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <img src="src/assets/Logo SMAIT.jpg" id="navbar-logo" />
-      <button id="navbar-home">home</button>
-      <button id="navbar-add">add student</button>
-      <button id="navbar-all">all students</button>
+    <div className="landing-page">
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="src/assets/logo-smait 1.png" id="navbar-logo" />
+          <p>STUDENT SYNC</p>
+        </div>
+        <div className="home">
+          <h5 className="navbar-home">home</h5>
+        </div>
+        <div className="add-student">
+          <h5 className="navbar-add">add student</h5>
+        </div>
+        <div className="all-student">
+          <h5 className="navbar-all">all students</h5>
+        </div>
+      </div>
     </div>
   );
 };
