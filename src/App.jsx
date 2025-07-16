7f0cfa3602d84314632dda5564be9ac4cc80ab67
gad-alt <hirwagad767@gmail.com>
2025-07-16 07:39:29 +0200

    feat: create html and css of hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StudentList from "./pages/StudentList";
+import HeroSection from "./components/hero";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <div className="app-container">
         <Navbar />
+        <HeroSection />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<StudentList />} />
---
+++ src/components/hero.jsx
@@ -0,0 +1,29 @@
+const HeroSection = () => {
+  return (
+    <div id="hero-container">
+      <div id="text-container">
+        <h3>Student management platform</h3>
+        <h1>STUDENTSYNC MANAGEMENT WEB APP</h1>
+        <p>student for academic year 2025/2026</p>
+        <button>learn more</button>
+        <button id="all-students">all students</button>
+      </div>
+      <div id="image-container">
+        <img
+          src="src/assets/hero_pics/54100351610_b2e6df9d55_o 1.png"
+          id="first-im"
+        />
+        <img src="src/assets/hero_pics/download 1.png" id="second-im" />
+        <img
+          src="src/assets/hero_pics/53998174142_660ae8a080_c 1.png"
+          id="third-im"
+        />
+        <img
+          src="src/assets/hero_pics/53999073171_9d71b8f1b6_k 2.png"
+          id="four-im"
+        />
+      </div>
+    </div>
+  );
+};
+export default HeroSection;
