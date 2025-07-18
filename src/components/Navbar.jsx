6f217fd3026054192f8b97a40cd9d3667098f31e
gad-alt <hirwagad767@gmail.com>
2025-07-18 17:04:14 +0200

    feat: add footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <div className="landing-page">
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="src/assets/logo-smait 1.png" id="navbar-logo" />
+          <p>STUDENT SYNC</p>
+        </div>
+        <div className="home">
+          <h5 className="navbar-home">home</h5>
+        </div>
+        <div className="add-student">
+          <h5 className="navbar-add">add student</h5>
+        </div>
+        <div className="all-student">
+          <h5 className="navbar-all">all students</h5>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Navbar;
