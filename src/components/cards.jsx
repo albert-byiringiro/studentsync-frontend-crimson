2e8fbf4f19620f2e9b3c1a533ca52e99125432dc
Naomie2003 <tuyishimenaome58@gmail.com>
2025-07-19 20:21:10 +0200

    feat: Improve performance

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -9,16 +9,16 @@ function Cards() {
         <div className="allCards">
           <div className="firstCard">
             <div className="hp1">
-              <p>
-                <img
-                  src="src/assets/hand-heart-line 1.png"
-                  alt="Transformation image"
-                  className="img1"
-                />
+              <img
+                src="src/assets/hand-heart-line 1.png"
+                alt="Transformation image"
+                className="img1"
+              />
+              <p className="firstClassP1">
                 <b>Digital transformation</b>
               </p>
               <br />
-              <p>
+              <p className="firstClassP">
                 Digital transformation emporwers students with tools and skills
                 that extended beyond technical proficiency fostering and
                 learning environment where ethical digital citizenship and
@@ -30,15 +30,13 @@ function Cards() {
           <br />
           <div className="secondCard">
             <div className="hp1">
+              <img src="src/assets/Frame 362.png" className="img2" />
               <p className="secondClassP1">
-                <b>
-                  <img src="src/assets/Frame 362.png" className="img2" />
-                  Discipline and productive
-                </b>
+                <b>Discipline and productive</b>
               </p>
               <br />
-              <br />
-              <p className="secondClassP">
+
+              <p>
                 Discipline in time as well as displine in all matters and
                 possessions.Organized and able to manage their agenda and
                 activities so that all the times can be valuable and beneficial
@@ -51,7 +49,6 @@ function Cards() {
                 <img src="src/assets/Frame 363 (1).png" className="image3" />
                 <b>Race toward goodness</b>
               </p>
-              <br />
 
               <p>
                 Compenting and striving in a goodness having speed and accuracy
@@ -66,3 +63,75 @@ function Cards() {
   );
 }
 export default Cards;
+//   margin-top:-42px ;
+//   margin-left: 50px;
+//   font-size:18px;
+//   line-height:100%
+
+// //   padding: 25px;
+//   width: 350px;
+//   height: 220px;
+//   transition: box-shadow 0.3s ease;
+//   margin-left: -70px;
+
+// }
+// .thirdCard{
+//   margin-top: -260px;
+//   margin-left: 750px;
+//   width: 350px;
+//   height: 220px;
+
+// }
+
+// .thirdCard-image {
+//   width: 40px;
+//   height: 40px;
+//   margin-bottom: 8px;
+//   object-fit: cover;
+//   border-radius: 8px;
+//   margin-top: px;
+// }
+// .secondCard-image2{
+//    width: 44px;
+//   height: 42px;
+//   margin-bottom: 8px;
+//   object-fit: cover;
+//   border-radius: 8px;
+//   margin-top: -5px;
+//   margin-left: -5px;
+// }
+// .image2-p1{
+//   margin-top:-42px ;
+//   margin-left: 50px;
+//   font-size:18px;
+//   line-height:100%
+
+// }
+// .image2-p2{
+//   margin-top:40px;
+//   margin-left:-5px;
+//   font-family:'Times New Roman', Times, serif;
+//   font-size: 15px;
+//   font-weight: 100;
+// }
+// .img1-p1{
+//   margin-top:-30px;
+//   margin-left:70px ;
+
+// }
+// .img1-p2{
+//   margin-top:25px;
+//   margin-left:-5px ;
+//   font-family:'Times New Roman', Times, serif;
+//    font-size: 14px;
+// }
+// .img3-p1{
+//   margin-top:-50px;
+//   margin-left:70px;
+//   font-size: 14px;
+// }
+// .img3-p2{
+// margin-top:20px;
+//   margin-left:-5px;
+//   font-size: 12px;
+// }
